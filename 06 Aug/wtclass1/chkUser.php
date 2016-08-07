13beb8c1a20ce3e89e82da0dae026863a80cc72f
doodhwala <mohit.surana95@gmail.com>
2016-08-07 10:19:03 +0530

    Initial Commit

---
+++ 06 Aug/wtclass1/chkUser.php
@@ -0,0 +1,16 @@
+<?php
+	header("Content-type:text/html");
+	extract($_GET);
+	// extract($_POST);
+	$used = 0;
+	$usernames = ["doodh", "doodhwala", "doodhwa!a", "mohit", "pavan"];
+	foreach ($usernames as $index => $username)
+	{
+		if($username == $user)
+		{
+			$used = 1;
+			break;
+		}
+	}
+	echo '<html><head><script>parent.usernameResponse( "' . $user . '", ' . $used . ')</script></head></html>';
+?>
---
+++ 06 Aug/wtclass1/submit.php
@@ -0,0 +1,3 @@
+<?php
+	echo '<html><body><h3>Details saved!</h3><body></html>';
+?>
