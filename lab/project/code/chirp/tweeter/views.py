ed3d147cadbd1d60ffdb3c95c2ecd3954f0b23aa
doodhwala <mohit.surana95@gmail.com>
2016-11-25 13:59:09 +0530

    Moved out lab project

--- lab/project/code/chirp/tweeter/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-# Create your views here.
-
-from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse("Chirp! This page is currently under development. Please come back later! :)")
